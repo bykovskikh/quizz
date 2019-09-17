88b9d792f697095e8cd43a54886db0e24e9853a6
Dima Bykovskikh <d.bykovskikh@gmail.com>
2019-09-17 18:12:44 +0300

    Updates

---
+++ precache-manifest.e69787c7c187da054cc8659f72247361.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "680a10a81f525eeccb5e",
+    "url": "/static/js/main.8bec3ece.chunk.js"
+  },
+  {
+    "revision": "5cb821b5f6e243d26531",
+    "url": "/static/js/2.4635dbdd.chunk.js"
+  },
+  {
+    "revision": "680a10a81f525eeccb5e",
+    "url": "/static/css/main.b899a129.chunk.css"
+  },
+  {
+    "revision": "5cb821b5f6e243d26531",
+    "url": "/static/css/2.9b413ad5.chunk.css"
+  },
+  {
+    "revision": "4665dd1403a04cd63a937e86c8183372",
+    "url": "/index.html"
+  }
+];
